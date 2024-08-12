3e94266f298e403131eeb6f4d540f31b00affdd9
Lucas <gelenskilucas@gmail.com>
2024-08-12 10:29:37 -0300

    Teste de conexão

--- server.js
+++ server.js
@@ -11,4 +11,15 @@ app.listen(3000, () => {
 const connection = mysql.createConnection({
   host: "localhost",
   user: "user_bd_tasks",
+  password: "QL0P4TDcQGB2R97Djet7vXYHggatTZE4",
+  database: "nodejs_tasks",
+});
+
+// Testar conexão
+connection.connect((error) => {
+  if (error) {
+    console.log("Erro na conexão ao MySQL: " + error.message);
+    return;
+  }
+  console.log("Conexão bem-sucedida.");
 });
